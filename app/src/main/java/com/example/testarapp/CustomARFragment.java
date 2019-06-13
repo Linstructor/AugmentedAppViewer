045f971dfa6fe0af19a1392bf6d1f3cecef7f28c
Tristan <tristan.morisot@viacesi.fr>
2019-06-13 15:00:14 +0200

    init commit

---
+++ app/src/main/java/com/example/testarapp/CustomARFragment.java
@@ -0,0 +1,91 @@
+package com.example.testarapp;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.ux.ArFragment;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CustomARFragment extends ArFragment {
+
+    private String TAG = "ARFrag";
+
+    private static final String DEFAULT_IMAGE_NAME = "frame.jpg";
+
+    private static final boolean USE_SINGLE_IMAGE = true;
+
+    private static final String SAMPLE_IMAGE_DATABASE = "images.imgdb";
+
+    @Override
+    protected Config getSessionConfiguration(Session session) {
+        getPlaneDiscoveryController().setInstructionView(null);
+        Config config = new Config(session);
+        config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+        session.configure(config);
+        getArSceneView().setupSession(session);
+        if (setupAugmentedImageDatabase(config, session)) {
+            Log.d("SetupAugImgDb", "Success");
+        } else {
+            Log.e("SetupAugImgDb","Faliure setting up db");
+        }
+        return config;
+    }
+
+    private boolean setupAugmentedImageDatabase(Config config, Session session) {
+        AugmentedImageDatabase augmentedImageDatabase;
+
+        AssetManager assetManager = getContext() != null ? getContext().getAssets() : null;
+        if (assetManager == null) {
+            Log.e(TAG, "Context is null, cannot intitialize image database.");
+            return false;
+        }
+
+        // There are two ways to configure an AugmentedImageDatabase:
+        // 1. Add Bitmap to DB directly
+        // 2. Load a pre-built AugmentedImageDatabase
+        // Option 2) has
+        // * shorter setup time
+        // * doesn't require images to be packaged in apk.
+        if (USE_SINGLE_IMAGE) {
+            Bitmap augmentedImageBitmap = loadAugmentedImageBitmap(assetManager);
+            if (augmentedImageBitmap == null) {
+                return false;
+            }
+
+            augmentedImageDatabase = new AugmentedImageDatabase(session);
+            augmentedImageDatabase.addImage(DEFAULT_IMAGE_NAME, augmentedImageBitmap);
+            // If the physical size of the image is known, you can instead use:
+            //     augmentedImageDatabase.addImage("image_name", augmentedImageBitmap, widthInMeters);
+            // This will improve the initial detection speed. ARCore will still actively estimate the
+            // physical size of the image as it is viewed from multiple viewpoints.
+        } else {
+            // This is an alternative way to initialize an AugmentedImageDatabase instance,
+            // load a pre-existing augmented image database.
+            try (InputStream is = getContext().getAssets().open(SAMPLE_IMAGE_DATABASE)) {
+                augmentedImageDatabase = AugmentedImageDatabase.deserialize(session, is);
+            } catch (IOException e) {
+                Log.e(TAG, "IO exception loading augmented image database.", e);
+                return false;
+            }
+        }
+
+        config.setAugmentedImageDatabase(augmentedImageDatabase);
+        return true;
+    }
+
+    private Bitmap loadAugmentedImageBitmap(AssetManager assetManager) {
+        try (InputStream is = assetManager.open(DEFAULT_IMAGE_NAME)) {
+            return BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            Log.e(TAG, "IO exception loading augmented image bitmap.", e);
+        }
+        return null;
+    }
+}
